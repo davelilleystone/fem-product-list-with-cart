a67d6269eae9148dce4ae0917510fa71fb02ef40
Dave Lilleystone <davelilleystone@gmail.com>
2025-01-20 18:08:57 +0000

    Commit before new branch

--- src/ts/app.ts
+++ src/ts/app.ts
@@ -4,3 +4,9 @@ const productRoot = document.querySelector(".product-container");
 
 // create product cards and add to the DOM
 productRoot?.insertAdjacentHTML("afterbegin", createProducts());
+
+const productCards = document.querySelectorAll(".product-card");
+
+console.log(productCards);
+
+productRoot?.addEventListener("click", (evt) => console.log(evt));
