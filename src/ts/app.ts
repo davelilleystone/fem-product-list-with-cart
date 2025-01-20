8115ffe22153308dc49118e8fd8e250f1e24b187
Dave Lilleystone <davelilleystone@gmail.com>
2025-01-20 17:48:02 +0000

    Add createProducts function that creates each card and returns a string of HTML that can then be inserted into the DOM

--- src/ts/app.ts
+++ src/ts/app.ts
@@ -1,2 +1,6 @@
-// Main TypeScript file
-console.log("app.js loaded");
+import createProducts from "./createProducts.js";
+
+const productRoot = document.querySelector(".product-container");
+
+// create product cards and add to the DOM
+productRoot?.insertAdjacentHTML("afterbegin", createProducts());
---
+++ src/ts/createProducts.ts
@@ -0,0 +1,35 @@
+import data from "./data.js";
+
+const createProducts = () => {
+  return data
+    .map((product) => {
+      const {
+        name: title,
+        category,
+        price,
+        image: { mobile, tablet, desktop },
+      } = product;
+      return `
+      <article class="product-card">
+        <picture class="product-card__image">
+          <source srcset=${mobile} media="(max-width: 480px)">
+          <source srcset=${tablet} media="(max-width: 768px)">
+          <img src=${desktop} alt="${title}" class="product-card__img">
+        </picture>
+        <div class="product-card__buttons">
+          <button class="product-card__button product-card__button--increment">+</button>
+          <button class="product-card__button product-card__button--add">Add to Cart</button>
+          <button class="product-card__button product-card__button--decrement">-</button>
+        </div>
+        <div class="product__details">
+          <span class="product-card__category">${category}</span>
+          <h2 class="product-card__title">${title}</h2>
+          <span class="product__price">$${Number(price).toFixed(2)}</span>
+        </div>
+      </article>
+    `;
+    })
+    .join("");
+};
+
+export default createProducts;
