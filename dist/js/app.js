4be36b9398b156b811c95b1fea5d3c24a75d72d6
Dave Lilleystone <davelilleystone@gmail.com>
2025-01-20 18:16:39 +0000

    Commit after strange issue where files that were tracked were not being committed and untracked files were not being moved to staging

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,4 +1,8 @@
-"use strict";
-// Main TypeScript file
-console.log("app.js loaded");
+import createProducts from "./createProducts.js";
+const productRoot = document.querySelector(".product-container");
+// create product cards and add to the DOM
+productRoot === null || productRoot === void 0 ? void 0 : productRoot.insertAdjacentHTML("afterbegin", createProducts());
+const productCards = document.querySelectorAll(".product-card");
+console.log(productCards);
+productRoot === null || productRoot === void 0 ? void 0 : productRoot.addEventListener("click", (evt) => console.log(evt));
 //# sourceMappingURL=app.js.map
