cfbc505d4739e171e367d0f84af8dba0b84681ee
Dave Lilleystone <davelilleystone@gmail.com>
2025-01-20 17:53:14 +0000

    Add id field to each item in data.js

--- src/ts/data.ts
+++ src/ts/data.ts
@@ -9,6 +9,7 @@ const data = [
     name: "Waffle with Berries",
     category: "Waffle",
     price: 6.5,
+    id: 1,
   },
   {
     image: {
@@ -20,6 +21,7 @@ const data = [
     name: "Vanilla Bean Crème Brûlée",
     category: "Crème Brûlée",
     price: 7.0,
+    id: 2,
   },
   {
     image: {
@@ -31,6 +33,7 @@ const data = [
     name: "Macaron Mix of Five",
     category: "Macaron",
     price: 8.0,
+    id: 3,
   },
   {
     image: {
@@ -42,6 +45,7 @@ const data = [
     name: "Classic Tiramisu",
     category: "Tiramisu",
     price: 5.5,
+    id: 4,
   },
   {
     image: {
@@ -53,6 +57,7 @@ const data = [
     name: "Pistachio Baklava",
     category: "Baklava",
     price: 4.0,
+    id: 5,
   },
   {
     image: {
@@ -64,6 +69,7 @@ const data = [
     name: "Lemon Meringue Pie",
     category: "Pie",
     price: 5.0,
+    id: 6,
   },
   {
     image: {
@@ -75,6 +81,7 @@ const data = [
     name: "Red Velvet Cake",
     category: "Cake",
     price: 4.5,
+    id: 7,
   },
   {
     image: {
@@ -86,6 +93,7 @@ const data = [
     name: "Salted Caramel Brownie",
     category: "Brownie",
     price: 4.5,
+    id: 8,
   },
   {
     image: {
@@ -97,6 +105,7 @@ const data = [
     name: "Vanilla Panna Cotta",
     category: "Panna Cotta",
     price: 6.5,
+    id: 9,
   },
 ];
 
