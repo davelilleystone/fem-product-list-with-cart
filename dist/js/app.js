220973d1057439a5a1b64d37d68940e67710b3e2
Dave Lilleystone <davelilleystone@gmail.com>
2025-01-22 10:06:52 +0000

    Simplify event handling in handleProductButtonClick, now just two functions to handle adding or incrementing / decrementing

--- dist/js/app.js
+++ dist/js/app.js
@@ -3,12 +3,44 @@ const productRoot = document.querySelector(".product-container");
 // create product cards and add to the DOM
 productRoot === null || productRoot === void 0 ? void 0 : productRoot.insertAdjacentHTML("afterbegin", createProducts());
 const productCards = document.querySelectorAll(".product-card");
+const handleAddToCart = (product) => {
+    console.log("add product to cart");
+    console.log(product);
+};
+// const handleIncrementProductQuantity = () => {
+//   console.log("inc product");
+// };
+// const handleDecrementProductQuantity = () => {
+//   console.log("dec product");
+// };
+const handleUpdateCart = (action, product) => {
+    console.log("Update cart: " + action);
+    console.log(product);
+};
 const handleProductBtnClick = (evt) => {
+    // check to make sure that it is actually a button being clicked within the product card
+    if (!evt.target.matches("button")) {
+        console.log("not a button!");
+        return;
+    }
     const clicked_btn = evt.target;
     const clicked_btn_type = clicked_btn.dataset.btn;
     const product = evt.currentTarget;
-    const productId = evt.currentTarget.dataset.productId;
-    console.log({ clicked_btn, clicked_btn_type, product, productId });
+    if (!clicked_btn.matches("button")) {
+        console.log("not a button!");
+        return;
+    }
+    switch (clicked_btn_type) {
+        case "add":
+            handleAddToCart(product);
+            break;
+        case "inc":
+        case "dec":
+            handleUpdateCart(clicked_btn_type, product);
+            break;
+        default:
+            throw Error("Unknown Button Type");
+    }
 };
 productCards.forEach((card) => card.addEventListener("click", handleProductBtnClick));
 //# sourceMappingURL=app.js.map
--- src/ts/app.ts
+++ src/ts/app.ts
@@ -7,13 +7,51 @@ productRoot?.insertAdjacentHTML("afterbegin", createProducts());
 
 const productCards = document.querySelectorAll(".product-card");
 
-const handleProductBtnClick = (evt) => {
+const handleAddToCart = (product: HTMLElement) => {
+  console.log("add product to cart");
+  console.log(product);
+};
+
+// const handleIncrementProductQuantity = () => {
+//   console.log("inc product");
+// };
+
+// const handleDecrementProductQuantity = () => {
+//   console.log("dec product");
+// };
+
+const handleUpdateCart = (action: "inc" | "dec", product: HTMLElement) => {
+  console.log("Update cart: " + action);
+  console.log(product);
+};
+
+const handleProductBtnClick = (evt: MouseEvent) => {
+  // check to make sure that it is actually a button being clicked within the product card
+  if (!evt.target.matches("button")) {
+    console.log("not a button!");
+    return;
+  }
+
   const clicked_btn = evt.target;
   const clicked_btn_type = clicked_btn.dataset.btn;
   const product = evt.currentTarget;
-  const productId = evt.currentTarget.dataset.productId;
 
-  console.log({ clicked_btn, clicked_btn_type, product, productId });
+  if (!clicked_btn.matches("button")) {
+    console.log("not a button!");
+    return;
+  }
+
+  switch (clicked_btn_type) {
+    case "add":
+      handleAddToCart(product);
+      break;
+    case "inc":
+    case "dec":
+      handleUpdateCart(clicked_btn_type, product);
+      break;
+    default:
+      throw Error("Unknown Button Type");
+  }
 };
 
 productCards.forEach((card) =>
